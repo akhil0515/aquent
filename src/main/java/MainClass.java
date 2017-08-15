7d7ee42b8c8e2e4bef171116d765027a31c74b38
DESKTOP-356U2O4\akhil <akhilreddy0515@gmail.com>
2017-08-15 19:41:48 -0400

    changes
    Handled the Exception and adding an executable jar for convinience

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -2,16 +2,29 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class MainClass {
-    public static void main(String[] args) throws IOException  {    	
-   	String input = args[0];
+    public static void main(String[] args) throws IOException {   
+    	try{
+    	
+    	String input = args[0];
     	String output = args[1];
-//    	String dummy = "C:\\Users\\kaush\\Downloads\\op.txt";
-//    	String dataUrl = "C:\\Users\\kaush\\Downloads\\Telegram Desktop\\sample.txt";
+        
+    	
     	AbstractClass s = new TextFile();
     	try {
 			s.call(input, output);
-		}catch (FileNotFoundException e){
+		
+    	}catch (FileNotFoundException e){
     		System.out.println("file not found in the given input location");
 		}
+    	}catch(ArrayIndexOutOfBoundsException ae){
+			System.out.println("Please enter the arguments");
+		}
+    
+    	   	
+    	
     }
-}
+ 		
+        
+    
+    }
+    
